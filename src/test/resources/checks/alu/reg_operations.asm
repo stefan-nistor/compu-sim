2d68f516a677d7f85ef064999712efc2d0b74d13
Vlad-Andrei Loghin <vlad.loghin@sonarsource.com>
2024-05-10 16:43:44 +0200

    Tester tests

--- src/test/resources/checks/alu/reg_operations.asm
+++ src/test/resources/checks/alu/reg_operations.asm
@@ -1,5 +1,6 @@
 // sim-test
 // expected: success
 
-mov r0 #10 // expect-true {r0==9}
-mov r1 #20 // expect-true {r0==5; r1==15}
+mov r0 #10 // expect-true {r0==10}
+add r0 #5
+mov r1 #20 // expect-true {r0==15; r1==20}
