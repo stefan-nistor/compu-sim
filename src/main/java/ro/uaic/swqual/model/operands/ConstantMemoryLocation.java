bd238add52afd4c6fe256c9f77f0db9b83f3ce46
Vlad-Andrei Loghin <vlad.loghin00@gmail.com>
2024-05-11 02:00:37 +0200

    Basic implementation of address parse in asm

--- src/main/java/ro/uaic/swqual/model/operands/ConstantMemoryLocation.java
+++ src/main/java/ro/uaic/swqual/model/operands/ConstantMemoryLocation.java
@@ -1,7 +1,14 @@
 package ro.uaic.swqual.model.operands;
 
+import java.util.Map;
+
 public class ConstantMemoryLocation extends MemoryLocation {
     public ConstantMemoryLocation(char value) {
         this.value = value;
+    }
+
+    @Override
+    public void resolveInnerReferences(Map<String, Register> registerMap) {
+        // nothing to resolve
     }
 }
