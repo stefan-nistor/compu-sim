fca468eecc6a467ae3fc88484ad7806144fc98f4
Vlad-Andrei Loghin <vlad.loghin@sonarsource.com>
2024-05-13 16:58:17 +0200

    Added reg update

--- src/main/java/ro/uaic/swqual/swing/CodeInputPanel.java
+++ src/main/java/ro/uaic/swqual/swing/CodeInputPanel.java
@@ -2,7 +2,6 @@ package ro.uaic.swqual.swing;
 
 import ro.uaic.swqual.Parser;
 import ro.uaic.swqual.model.Instruction;
-import ro.uaic.swqual.proc.CentralProcessingUnit;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -21,10 +20,20 @@ public class CodeInputPanel extends JPanel {
     private JTextArea codeArea;
     private JButton runButton;
     private JButton chooseFileButton;
-    private JTextArea registryView;
     private JTextArea textArea3;
     private JTextPane textPane1;
     private JTextArea textArea4;
+    private JLabel r0Val;
+    private JLabel r1Val;
+    private JLabel r2Val;
+    private JLabel r3Val;
+    private JLabel r4Val;
+    private JLabel r5Val;
+    private JLabel r6Val;
+    private JLabel r7Val;
+    private JLabel spVal;
+    private JLabel pcVal;
+    private JLabel flVal;
 
     private static Parser parser = new Parser();
     private CpuOrchestrator cpuOrchestrator;
@@ -77,7 +86,7 @@ public class CodeInputPanel extends JPanel {
         chooseFileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser("/home/loghin/IdeaProjects/compu-sim/src/test/resources/checks");
+                JFileChooser fileChooser = new JFileChooser();
                 int option = fileChooser.showOpenDialog(SwingUtilities.getWindowAncestor(chooseFileButton));
                 if (option == JFileChooser.APPROVE_OPTION) {
                     String selectedFilePath = fileChooser.getSelectedFile().getAbsolutePath();
@@ -119,7 +128,17 @@ public class CodeInputPanel extends JPanel {
     }
 
     public void update() {
-
+        r0Val.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getDataRegisters().get(0).getValue(), 16));
+        r1Val.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getDataRegisters().get(1).getValue(), 16));
+        r2Val.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getDataRegisters().get(2).getValue(), 16));
+        r3Val.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getDataRegisters().get(3).getValue(), 16));
+        r4Val.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getDataRegisters().get(4).getValue(), 16));
+        r5Val.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getDataRegisters().get(5).getValue(), 16));
+        r6Val.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getDataRegisters().get(6).getValue(), 16));
+        r7Val.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getDataRegisters().get(7).getValue(), 16));
+        spVal.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getStackPointer().getValue(), 16));
+        pcVal.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getProgramCounter().getValue(), 16));
+        flVal.setText(Integer.toString(cpuOrchestrator.getCentralProcessingUnit().getFlagRegister().getValue(), 16));
     }
 
     public static void main(String[] args) {
@@ -133,6 +152,7 @@ public class CodeInputPanel extends JPanel {
         var orch = new CpuOrchestrator(Map.of());
         codeInputPanel.setCpuOrchestrator(orch);
         orch.addUpdateListener(codeInputPanel::update);
+        codeInputPanel.update();
 
         frame.addWindowListener(new WindowAdapter() {
             @Override
