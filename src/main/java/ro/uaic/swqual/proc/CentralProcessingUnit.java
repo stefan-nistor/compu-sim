c75a40fda4c139b2b691b0a273b9dc80da126f77
Vlad-Andrei Loghin <vlad.loghin00@gmail.com>
2024-05-13 07:31:14 +0200

    Registry state reset at relevant instructions. Complex test for shits & giggles

--- src/main/java/ro/uaic/swqual/proc/CentralProcessingUnit.java
+++ src/main/java/ro/uaic/swqual/proc/CentralProcessingUnit.java
@@ -1,5 +1,9 @@
 package ro.uaic.swqual.proc;
 
+import ro.uaic.swqual.exception.InstructionException;
+import ro.uaic.swqual.exception.ParameterException;
+import ro.uaic.swqual.model.Instruction;
+import ro.uaic.swqual.model.InstructionType;
 import ro.uaic.swqual.model.operands.FlagRegister;
 import ro.uaic.swqual.model.operands.Register;
 
@@ -19,6 +23,12 @@ public class CentralProcessingUnit extends DelegatingUnit {
 
     private final Map<String, Register> registryReferenceMap = new HashMap<>();
 
+    private void prepareStateBefore(InstructionType instructionType) {
+        if (!InstructionType.isIpuInstruction(instructionType)) {
+            flagRegister.clear();
+        }
+    }
+
     public CentralProcessingUnit() {
         IntStream.range(0, 8).forEach(regIndex -> {
             dataRegisters.add(new Register());
@@ -27,6 +37,12 @@ public class CentralProcessingUnit extends DelegatingUnit {
 
         registryReferenceMap.put("sp", stackPointer);
         registryReferenceMap.put("pc", programCounter);
+    }
+
+    @Override
+    public void execute(Instruction instruction) throws InstructionException, ParameterException {
+        prepareStateBefore(instruction.getType());
+        super.execute(instruction);
     }
 
     @Override public void raiseFlag(char value) {
---
+++ src/test/resources/checks/misc/count_prime_in_array.asm
@@ -0,0 +1,105 @@
+// sim-test
+// expected: success
+
+jmp @main;
+
+
+@sum:
+mov r0 [sp - 6];
+add r0 [sp - 4];
+ret;
+
+
+@square:
+mov r0 [sp - 4];
+umul r0 r0;
+ret;
+
+
+@cube:
+push [sp - 4];
+call @square;
+pop;
+umul r0 [sp - 4];
+ret;
+
+
+@isPrime:
+mov r3 [sp - 4];
+
+// if < 2, is not prime
+mov r0 0;
+cmp r3 2;
+jlt @isPrimeEnd;
+
+// if < 4, implicitly prime
+mov r0 1;
+
+mov r2 2;
+@isPrimeLoop:
+mov r4 r2;
+umul r4 r4;
+cmp r4 r3;
+jgt @isPrimeEnd;
+
+// compute remainder
+mov r1 r3;
+udiv r1 r2;
+cmp r7 0;
+// if not zero, keep going
+jne @isPrimeContinue;
+// if zero, not prime.
+mov r0 0;
+jmp @isPrimeEnd;
+
+@isPrimeContinue:
+add r2 1;
+jmp @isPrimeLoop;
+
+@isPrimeEnd:
+ret;
+
+
+// Input[kb]: n, then n numbers
+// Output[r0]: number of prime numbers in given sequence of n numbers
+
+
+// kb-preload {6, 11, 19, 25, 14, 97, 13}
+@expectResult:
+mov r0 [sp - 4]; // expect-true {r0 == 4}
+ret;
+
+
+@main:
+
+mov r5 [0x10];
+umul r5 2;
+mov r6 0;
+@readNumsLoopStart:
+mov [0x600 + r6] [0x10];
+add r6 2;
+cmp r6 r5;
+jlt @readNumsLoopStart;
+
+// parse nums
+mov r6 0;
+mov r4 r5;
+mov r5 0;
+@computeHowManyPrimeLoopStart:
+push r4;
+push [0x600 + r6];
+call @isPrime;
+pop;
+pop r4;
+cmp r0 1;
+jne @computeHowManyPrimeLoopContinue;
+add r5 1;
+
+@computeHowManyPrimeLoopContinue:
+add r6 2;
+cmp r6 r4;
+jlt @computeHowManyPrimeLoopStart;
+
+push r5;
+call @expectResult;
+pop;
